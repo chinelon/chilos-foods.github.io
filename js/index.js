9b8794de69b4596bffdd4bd6e7228056821effa9
chinelon <72803021+chinelon@users.noreply.github.com>
2023-11-25 22:31:14 +0100

    added external links

---
+++ js/index.js
@@ -0,0 +1,7 @@
+var Menu = document.getElementById('button3');
+
+        // Add a click event listener to the button
+        Menu.addEventListener('click', function() {
+            // Redirect to the PDF file
+            window.location.href = '../assets/chilos-Menu.pdf';
+        });
