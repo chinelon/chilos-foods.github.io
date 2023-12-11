5d75919384591846508563dd814a68d259c8c188
chinelon <72803021+chinelon@users.noreply.github.com>
2023-12-11 10:38:50 +0100

    js

--- js/index.js
+++ js/index.js
@@ -3,5 +3,5 @@ var Menu = document.getElementById('button3');
         // Add a click event listener to the button
         Menu.addEventListener('click', function() {
             // Redirect to the PDF file
-            window.location.href = '../assets/chilos-Menu.pdf';
+            window.location.href = './assets/chilos-Menu.pdf';
         });
